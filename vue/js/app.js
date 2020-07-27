04dafa8b8debee538995ee52587bb4be99821a60
ElizabethCA94 <ecarreno@unal.edu.co>
2020-07-26 23:27:53 -0500

    create a v-bind class error

--- vue/js/app.js
+++ vue/js/app.js
@@ -6,10 +6,11 @@ const app = new Vue({
         isVisible: false, 
         people: ['andres', 'juan', 'pepe'],
         content_html: "<h1>Hola mundo</h1>",
-        src: "https://cnnespanol2.files.wordpress.com/2019/12/s_64a163f16ecbb099e52f2f8271f73cbbfcfc9034be4d646f7375e4db1ca6f3d7_1573501883482_ap_19001106049831-1.jpg?quality=100&strip=info&w=320&h=240&crop=1",
+        src: "https://imagenmix.net/wp-content/uploads/2016/09/imagenes-bonitas.jpg",
         text: "Descripcion de nuestra imagen",
         numero: 20,
-        v_model: "Hola v-model"
+        v_model: "Hola v-model",
+        p_error: false
     },
     computed: {
         // a computed getter
